404b779f877292cebca528af8286e528157d3a06
binodsuman <binodsuman@github.com>
2023-10-21 10:42:01 +0530

    Adding scroll down arrow and animation over there

--- script.js
+++ script.js
@@ -1,130 +1,63 @@
-//document.addEventListener("DOMContentLoaded", function () {
-    // Example course data
+let currentSection = 0;
 
-    const homeSection = document.getElementById("home");
-    const aboutSection = document.getElementById("about");
-    const latsetSection = document.getElementById("latest");
-    const mostSection = document.getElementById("most");
-        
-        aboutSection.style.display = "none";
-        latsetSection.style.display = "none";
-        mostSection.style.display = "none";
-      
-    const mostWatchedCourses = [
-        { title: "Data Science", description: "Convolutional Neural Networks | CNN | Kernel | Stride | Padding | Pooling | Flatten | Formula", id: "Y1qxI-Df4Lk" },
-        { title: "Data Engineering", description: "Box Plot - 1 | How to draw Box Plot and Outlier | Data Mining | Statistics" , id: "sytBDWefYb0"},
-        { title: "Data Science", description: "Recurrent Neural Networks | RNN LSTM Tutorial | Why use RNN | On Whiteboard | Compare ANN, CNN, RNN", id: "KBftoy0DPxI" }
-        
-    ];
+        document.addEventListener("scroll", function () {
+            const sections = document.querySelectorAll("section");
+            const offset = window.innerHeight / 2;
 
-    const latestVideos = [
-        { title: "Data Science", description: "What is Vector Database | Milvus Attu Installation | Word Embedding | End to end Demo", id: "612Y0jXmWKk" },
-        { title: "NLP", description: "Introduction to NLP | Word2Vec | Word Embedding | Whiteboard explanation | Gensim Coding" , id: "9Ppg8NLk4NE"},
-        { title: "ChatGPT", description: "ChatGPT: A Game Changer for System Design", id: "yE3O28E38_E" }
-       
-    ];
-
-     // Function to display courses
-     function displayLatestVideos() {
-        console.log("displayLatestVideos() called");
-        //document.body.style.backgroundImage = 'url("youtube_thumbnail.png")';
-        document.body.style.animation = " moveBackground 0s linear infinite"; 
-        const coursesSection = document.getElementById("latest");
-        latestVideos.forEach(course => {
-            const courseDiv = document.createElement("div");
-            courseDiv.classList.add("course");
-            
-            courseDiv.innerHTML = `<h3>${course.title}</h3><p>${course.description}</p><iframe width="560" height="315" src="https://www.youtube.com/embed/${course.id}" frameborder="0" allowfullscreen></iframe>`;
-            coursesSection.appendChild(courseDiv);
+            sections.forEach((section, index) => {
+                if (section.getBoundingClientRect().top < offset) {
+                    currentSection = index;
+                }
+            });
         });
-    }
 
-    // Function to display courses
-    function displayMostVideos() {
-        console.log("displayMostVideos() called");
-        document.body.style.animation = " moveBackground 0s linear infinite"; 
-        
-        const coursesSection = document.getElementById("most");
-        mostWatchedCourses.forEach(course => {
-            const courseDiv = document.createElement("div");
-            courseDiv.classList.add("course");
-            
-            courseDiv.innerHTML = `<h3>${course.title}</h3><p>${course.description}</p><iframe width="560" height="315" src="https://www.youtube.com/embed/${course.id}" frameborder="0" allowfullscreen></iframe>`;
-            coursesSection.appendChild(courseDiv);
-        });
-    }
- 
-    // Call the function to display courses
-    //displayMostVideos();
-    //displayLatestVideos();
-    
-    
-
-
-    // Function to display the selected section and hide others
-    function showSection(sectionId) {
-        const sections = ['home','latest', 'about', 'most'];
-        sections.forEach(section => {
-            const element = document.getElementById(section);
-            if (section === sectionId) {
-                element.style.display = 'block';
-            } else {
-                element.style.display = 'none';
+        document.addEventListener("keydown", function (e) {
+            if (e.key === "ArrowDown" && currentSection < 2) {
+                currentSection++;
+                scrollToSection(currentSection);
+            } else if (e.key === "ArrowUp" && currentSection > 0) {
+                currentSection--;
+                scrollToSection(currentSection);
             }
         });
-    }
 
-    // Add click event handlers to the links
-    const latestLink = document.getElementById('latest-link');
-    const aboutLink = document.getElementById('about-link');
-    const mostLink = document.getElementById('most-link');
-    const homeLink = document.getElementById('home-link');
-    const about_me_Link = document.getElementById('about-me-link');
-    
-    //const youtubeLink = document.getElementById('youtube-link');
-
-    
-
-    homeLink.addEventListener('click', function (event) {
-        console.log("Home link called");
-        document.body.style.animation = " moveBackground 10s linear infinite"; 
-        event.preventDefault();
-        showSection('home');
-    });
-
-    latestLink.addEventListener('click', function (event) {
-        displayLatestVideos();
-        event.preventDefault();
-        showSection('latest');
-    });
-
-    aboutLink.addEventListener('click', function (event) {
-        document.body.style.animation = " moveBackground 10s linear infinite"; 
-        event.preventDefault();
-        showSection('about');
-    });
-
-    
-
-    mostLink.addEventListener('click', function (event) {
-        displayMostVideos();
-        event.preventDefault();
-        showSection('most');
-    });
-
-    function scrollThumbnails(direction) {
-        const container = document.querySelector('.thumbnails-container');
-        const scrollAmount = 200; // Adjust the scroll amount as needed
-    
-        if (direction === 'left') {
-            container.scrollBy(-scrollAmount, 0);
-        } else if (direction === 'right') {
-            container.scrollBy(scrollAmount, 0);
+        function scrollToSection(sectionIndex) {
+            const sections = document.querySelectorAll("section");
+            sections[sectionIndex].scrollIntoView({ behavior: "smooth" });
         }
-    }
-    
-    function openVideo(videoUrl) {
-        // Open the YouTube video in a new tab
-        window.open(videoUrl, '_blank');
-    }
-    
+
+        const hoverImage = document.getElementById('page-scroll');
+        
+        
+        hoverImage.addEventListener('mouseover', () => {
+            // Get the target section you want to scroll to
+            const targetSection = document.getElementById('section2'); // Change 'section2' to the ID of the desired section
+            
+            if (targetSection) {
+                // Calculate the target scroll position
+                const scrollPosition = targetSection.offsetTop;
+                
+                // Animate the scroll with a custom easing function
+                const start = window.scrollY;
+                const end = scrollPosition;
+                const duration = 4000; // Adjust this to control the speed (in milliseconds)
+
+                let startTime;
+                function step(timestamp) {
+                    if (!startTime) startTime = timestamp;
+                    const progress = timestamp - startTime;
+                    window.scrollTo(0, easeInOutCubic(progress, start, end - start, duration));
+                    if (progress < duration) {
+                        requestAnimationFrame(step);
+                    }
+                }
+                requestAnimationFrame(step);
+            }
+        });
+
+function easeInOutCubic(t, b, c, d) {
+    if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
+    return c / 2 * ((t -= 2) * t * t + 2) + b;
+}
+
+
---
+++ script2.js
@@ -0,0 +1,130 @@
+//document.addEventListener("DOMContentLoaded", function () {
+    // Example course data
+
+    const homeSection = document.getElementById("home");
+    const aboutSection = document.getElementById("about");
+    const latsetSection = document.getElementById("latest");
+    const mostSection = document.getElementById("most");
+        
+        aboutSection.style.display = "none";
+        latsetSection.style.display = "none";
+        mostSection.style.display = "none";
+      
+    const mostWatchedCourses = [
+        { title: "Data Science", description: "Convolutional Neural Networks | CNN | Kernel | Stride | Padding | Pooling | Flatten | Formula", id: "Y1qxI-Df4Lk" },
+        { title: "Data Engineering", description: "Box Plot - 1 | How to draw Box Plot and Outlier | Data Mining | Statistics" , id: "sytBDWefYb0"},
+        { title: "Data Science", description: "Recurrent Neural Networks | RNN LSTM Tutorial | Why use RNN | On Whiteboard | Compare ANN, CNN, RNN", id: "KBftoy0DPxI" }
+        
+    ];
+
+    const latestVideos = [
+        { title: "Data Science", description: "What is Vector Database | Milvus Attu Installation | Word Embedding | End to end Demo", id: "612Y0jXmWKk" },
+        { title: "NLP", description: "Introduction to NLP | Word2Vec | Word Embedding | Whiteboard explanation | Gensim Coding" , id: "9Ppg8NLk4NE"},
+        { title: "ChatGPT", description: "ChatGPT: A Game Changer for System Design", id: "yE3O28E38_E" }
+       
+    ];
+
+     // Function to display courses
+     function displayLatestVideos() {
+        console.log("displayLatestVideos() called");
+        //document.body.style.backgroundImage = 'url("youtube_thumbnail.png")';
+        document.body.style.animation = " moveBackground 0s linear infinite"; 
+        const coursesSection = document.getElementById("latest");
+        latestVideos.forEach(course => {
+            const courseDiv = document.createElement("div");
+            courseDiv.classList.add("course");
+            
+            courseDiv.innerHTML = `<h3>${course.title}</h3><p>${course.description}</p><iframe width="560" height="315" src="https://www.youtube.com/embed/${course.id}" frameborder="0" allowfullscreen></iframe>`;
+            coursesSection.appendChild(courseDiv);
+        });
+    }
+
+    // Function to display courses
+    function displayMostVideos() {
+        console.log("displayMostVideos() called");
+        document.body.style.animation = " moveBackground 0s linear infinite"; 
+        
+        const coursesSection = document.getElementById("most");
+        mostWatchedCourses.forEach(course => {
+            const courseDiv = document.createElement("div");
+            courseDiv.classList.add("course");
+            
+            courseDiv.innerHTML = `<h3>${course.title}</h3><p>${course.description}</p><iframe width="560" height="315" src="https://www.youtube.com/embed/${course.id}" frameborder="0" allowfullscreen></iframe>`;
+            coursesSection.appendChild(courseDiv);
+        });
+    }
+ 
+    // Call the function to display courses
+    //displayMostVideos();
+    //displayLatestVideos();
+    
+    
+
+
+    // Function to display the selected section and hide others
+    function showSection(sectionId) {
+        const sections = ['home','latest', 'about', 'most'];
+        sections.forEach(section => {
+            const element = document.getElementById(section);
+            if (section === sectionId) {
+                element.style.display = 'block';
+            } else {
+                element.style.display = 'none';
+            }
+        });
+    }
+
+    // Add click event handlers to the links
+    const latestLink = document.getElementById('latest-link');
+    const aboutLink = document.getElementById('about-link');
+    const mostLink = document.getElementById('most-link');
+    const homeLink = document.getElementById('home-link');
+    const about_me_Link = document.getElementById('about-me-link');
+    
+    //const youtubeLink = document.getElementById('youtube-link');
+
+    
+
+    homeLink.addEventListener('click', function (event) {
+        console.log("Home link called");
+        document.body.style.animation = " moveBackground 10s linear infinite"; 
+        event.preventDefault();
+        showSection('home');
+    });
+
+    latestLink.addEventListener('click', function (event) {
+        displayLatestVideos();
+        event.preventDefault();
+        showSection('latest');
+    });
+
+    aboutLink.addEventListener('click', function (event) {
+        document.body.style.animation = " moveBackground 10s linear infinite"; 
+        event.preventDefault();
+        showSection('about');
+    });
+
+    
+
+    mostLink.addEventListener('click', function (event) {
+        displayMostVideos();
+        event.preventDefault();
+        showSection('most');
+    });
+
+    function scrollThumbnails(direction) {
+        const container = document.querySelector('.thumbnails-container');
+        const scrollAmount = 200; // Adjust the scroll amount as needed
+    
+        if (direction === 'left') {
+            container.scrollBy(-scrollAmount, 0);
+        } else if (direction === 'right') {
+            container.scrollBy(scrollAmount, 0);
+        }
+    }
+    
+    function openVideo(videoUrl) {
+        // Open the YouTube video in a new tab
+        window.open(videoUrl, '_blank');
+    }
+    
